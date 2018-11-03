6eded7951c02ee0a4e0eb71afc6185dc1b91c52b
בר קזז <barkazzaz@gmail.com>
2018-11-03 20:59:36 +0200

    play button for party

--- WebApplication2/wwwroot/js/site.js
+++ WebApplication2/wwwroot/js/site.js
@@ -96,6 +96,6 @@ new ScrollHandler("two");
 new ScrollHandler("three");
 new ScrollHandler("four");
 
-function credsToggle() {
-    $("#credentials").toggle();
-}
+$('.playButton').click(function () {
+    $('#partyvid').append('<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1&autoplay=1"></iframe>');
+});
