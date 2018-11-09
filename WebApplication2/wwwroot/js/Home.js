dd2ab5aff82fd861b595de75aa4e8394f82ef6f4
Kfir <kfir5959@gmail.com>
2018-11-09 19:16:15 +0200

    Scroll fixing and adding like/dislike buttons

--- WebApplication2/wwwroot/js/Home.js
+++ WebApplication2/wwwroot/js/Home.js
@@ -4,3 +4,26 @@ $('#chillvid').append('<iframe width="420" height="345" src="https://www.youtube
 $('#ragevid').append('<iframe width="420" height="345" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1&autoplay=1"></iframe>');
 
 
+$('#likeParty').on("click", function () {
+    alert("like Party!");
+});
+
+$('#dislikeParty').on("click", function () {
+    alert("dislike Party!");
+});
+
+$('#likeChill').on("click", function () {
+    alert("like Chill!");
+});
+
+$('#dislikeChill').on("click", function () {
+    alert("dislike Chill!");
+});
+
+$('#likeRage').on("click", function () {
+    alert("like Rage!");
+});
+
+$('#dislikeRage').on("click", function () {
+    alert("dislike Rage!");
+});
